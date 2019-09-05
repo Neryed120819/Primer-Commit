36872b10234c34b9c9e3ab6871257c5280e9de6a
Neryed120819 <eduardo11.agr@gmail.com>
2019-09-05 10:17:25 -0500

    Version 2.0

--- estructura1.cpp
+++ estructura1.cpp
@@ -1,18 +1,35 @@
-# include <iostream>
+#include <iostream>
+#include <conio.h>
 using namespace std;
-//usando el espacio de nombres estándar
-struct  Animal {
-	char nombre [ 20 ];
-	int edad;
-} animal1, animal2;
+struct dueno{
+	char nombred[20];
+};
 
-int  main ( int argc, char** argv){
-	cout<<"Nombre: " ;
-	cin.getline(animal2. nombre , 21220, '\n');
-	cout << " Edad: " ;
-	cin >> animal2.edad ;
-	cout << animal2.nombre << endl;
-	cout << animal2.edad << endl;
-	system("pause");
-	return 0 ;
+struct mascota{
+	char nombre[20];
+	int edad;
+	struct dueno due;
+};
+
+int main(int argc, char** argv) {
+	struct mascota M1[5];
+	struct mascota*apuntador;
+	
+	apuntador=&M1[0];
+	
+	for (int i=0; i<1; i++){
+	cout<<"Nombre: ";
+	cin.getline(M1[i].nombre, 20, '\n' );
+	cout<<"Edad: ";
+	cin>>M1[i].edad;
+	cout<<"Nombre del duenio: ";
+	fflush(stdin);
+	cin.getline(M1[i].due.nombred, 20, '\n' );
+	}
+	//Mostrar lo guardado
+	cout<<"Nombre guardado: "<< apuntador->nombre<<endl;
+	cout<<"Edad guardada: "<< apuntador->edad<<endl;
+	cout<<"Nombre de duenio guardado: "<< apuntador->due.nombred<<endl;
+	getch();
+	return 0;
 }
