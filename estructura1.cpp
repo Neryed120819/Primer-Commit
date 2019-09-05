262d34a6828548b2b9639b7363fc56e05531c7e2
Neryed120819 <eduardo11.agr@gmail.com>
2019-09-05 10:13:25 -0500

    Primer-Commit

---
+++ estructura1.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+//usando el espacio de nombres estándar
+struct  Animal {
+	char nombre [ 20 ];
+	int edad;
+} animal1, animal2;
+
+int  main ( int argc, char** argv){
+	cout<<"Nombre: " ;
+	cin.getline(animal2. nombre , 21220, '\n');
+	cout << " Edad: " ;
+	cin >> animal2.edad ;
+	cout << animal2.nombre << endl;
+	cout << animal2.edad << endl;
+	system("pause");
+	return 0 ;
+}
